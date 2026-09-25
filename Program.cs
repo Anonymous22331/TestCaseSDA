fb2a0efe707767d1a5beb5ebe97a4a4dc41e704e
Anonymous <makar.sidorov@gmail.com>
2026-09-25 10:07:00 +0300

    Initialize project and create structure

---
+++ Program.cs
@@ -0,0 +1,29 @@
+
+namespace TestCaseSDA;
+
+public sealed class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddOpenApi();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.MapControllers();
+
+        app.Run();
+    }
+}
